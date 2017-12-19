e5e7488b2253dab34491656c5273c2d7c640c43f
fwfly <fawafwfly@gmail.com>
2017-12-19 14:41:36 +1100

    Init file
    Add geo helper to read elevation from bmp file.
    Return elevation from latitude, longitude

---
+++ geo_helper.py
@@ -0,0 +1,58 @@
+'''
+Geo helper is a module to analyze geoography from input bmp file by openCV
+'''
+
+import numpy as np
+import cv2
+
+
+ELE_PER_COLOR = 41.81 # 6900(height of Aconcagua)/165 (color on map)
+
+class GeoHelper:
+
+    def __init__(self):
+        self.img = None
+        self.img_height = 0
+        self.img_width = 0
+
+
+    def read_map( self, bmp_file ):
+        '''
+        read a bmp file and store it into memory
+
+        :param bmp_file: file name
+        :type bmp_file : string
+        '''
+        self.img = cv2.imread(bmp_file, 0)
+        self.img_height, self.img_width = self.img.shape
+
+        #print self.img
+        #print self.img_height, self.img_width
+
+    def get_elevation( self, lag, log ):
+        '''
+        map lag and log into img position and get elevation from rgb
+
+        param lag : latitude from -90 to 90
+        type lag : Int
+        param log : longitude from -180 to 180
+        type log : Int
+
+        return : meters of elevation
+        '''
+
+        y = 90 - lag
+        x = 180 + log
+        # change it into map position
+        y = int((y/180) * self.img_height)
+        x = int((x/360) * self.img_width)
+        return ELE_PER_COLOR * self.img[y, x]
+
+
+if __name__ == '__main__':
+    print "Test GeoHelper"
+    geo = GeoHelper()
+    #geo.read_map("fabkedata") # test fake data
+    geo.read_map("elevation.bmp")
+    print geo.get_elevation(-33.86, 151.21) # Sydney
+    print geo.get_elevation(-32.39,-70.0) # Aconcagua
---
+++ main.py
@@ -0,0 +1,23 @@
+import geo_helper
+
+GEO_MAP = "elevation.bmp"
+
+def main():
+    # init input data
+    # init elevation
+    geo = geo_helper()
+
+
+    # compute Temperature
+
+    # compute Pressure
+
+    # compute humidity
+
+    # compute weather status
+
+    # output weather
+
+
+if __name__ == '__main__':
+    main()
